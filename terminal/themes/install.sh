259820ec039d1b7d182967c3617c0b3514db6110
wistic <prathameshkatkar11@gmail.com>
2021-08-17 02:31:12 +0530

    Added themes and fonts

---
+++ terminal/themes/install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# necessary on ubuntu
+export TERMINAL=gnome-terminal
+
+# =================THEME INSTALLATION============================ #
+bash ./horizon-dark.sh
+bash ./material.sh
+bash ./omni.sh
+bash ./vs-code-dark-plus.sh
+# =============================================================== #
